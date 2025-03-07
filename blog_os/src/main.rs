2f8fb0336add09e3bb9f4eb643cb4ae2f76c4731
LeulShiferaw <abiyleoul3@gmail.com>
2025-03-07 13:58:24 -0600

    Remove exception from main and add to lib test

--- blog_os/src/main.rs
+++ blog_os/src/main.rs
@@ -30,10 +30,8 @@ fn trivial_assert() {
 pub extern "C" fn _start() -> ! {
     println!("Hello World {}\nWHEN\n", "!");
 
-    //Interrupts code
     blog_os::init();
 
-    //Invoking the interrupt
     x86_64::instructions::interrupts::int3();
 
     #[cfg(test)]
