b22ea02aa9be3eecca1c9f28ccde127826dde1e6
LeulShiferaw <abiyleoul3@gmail.com>
2025-03-14 15:17:39 -0500

    Add separate test for kernel_overflow:

--- blog_os/src/main.rs
+++ blog_os/src/main.rs
@@ -32,12 +32,6 @@ pub extern "C" fn _start() -> ! {
 
     blog_os::init();
 
-    fn stack_overflow() {
-        stack_overflow();
-    }
-
-    stack_overflow();
-
     #[cfg(test)]
     test_main();
 
