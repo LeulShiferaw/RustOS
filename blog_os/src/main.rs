9baf7bf456aa1717e52184e06d4972872d69d0ff
Leul Shiferaw <abiyleoul3@gmail.com>
2025-04-04 16:40:42 -0400

    Finish paging implementation
    In specific allocating new frames

--- blog_os/src/main.rs
+++ blog_os/src/main.rs
@@ -7,20 +7,30 @@
 use core::panic::PanicInfo;
 use blog_os::println;
 use bootloader::{BootInfo, entry_point};
-use x86_64::structures::paging::PageTable;
-use blog_os::memory::{translate_addr};
 
 entry_point!(kernel_main);
 
 fn kernel_main(boot_info: &'static BootInfo) -> ! {
-    use blog_os::memory::active_level_4_table;
-    use x86_64::VirtAddr;
+    use blog_os::memory;
+    use x86_64::{structures::paging::Page, VirtAddr};
+    use blog_os::memory::BootInfoFrameAllocator;
 
     println!("Hello World{}", "!");
     blog_os::init();
 
     let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
+    let mut mapper = unsafe { memory::init(phys_mem_offset) };
+    let mut frame_allocator = unsafe {
+        BootInfoFrameAllocator::init(&boot_info.memory_map)
+    };
 
+    let page = Page::containing_address(VirtAddr::new(0xdeadbeaf000));
+    memory::create_example_mapping(page, &mut mapper, &mut frame_allocator);
+
+    let page_ptr: *mut u64 = page.start_address().as_mut_ptr();
+    unsafe { page_ptr.offset(400).write_volatile(0x_f021_f077_f065_f04e)};
+
+    /*
     let addresses = [
         0xb8000, //VGA Buffer
         0x201008, //Some code page
@@ -30,9 +40,10 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
 
     for &address in &addresses {
         let virt = VirtAddr::new(address);
-        let phys = unsafe { translate_addr(virt, phys_mem_offset) };
+        let phys = mapper.translate(virt);
         println!("{:?} -> {:?}", virt, phys);
     }
+        */
 
     // as before
     #[cfg(test)]
